87e8d26c676b0c705c9f271d581aa41c28e86010
pluto <1799718517@qq.com>
2023-12-03 18:24:51 +0800

    版本4

---
+++ 第二章.c
@@ -0,0 +1,93 @@
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<ctype.h>
+//1
+// int main(void)
+// {
+//     printf("suguocheng\n");
+//     printf("su\nguocheng\n");
+//     printf("su");
+//     printf("guocheng");
+// }
+//4
+// void jolly(void);
+// void deny(void);
+// int main(void)
+// {
+//     jolly();
+//     deny();
+// }
+// void jolly(void)
+// {
+//     printf("for he is a jolly good fellow\n");
+//     printf("for he is a jolly good fellow\n");
+//     printf("for he is a jolly good fellow\n");
+// }
+// void deny(void)
+// {
+//     printf("which nobody can deny");
+// }
+//5
+// void br(void);
+// void ic(void);
+// int main(void)
+// {
+//     br();
+//     printf(", ");
+//     ic();
+//     printf("\n");
+//     ic();
+//     printf(",\n");
+//     br();
+// }
+// void br(void)
+// {
+//     printf("brazil,russia");
+// 
+//6
+// int main(void)
+// {
+//     int toes=10;
+//     printf("toes=%d\n",toes);
+//     printf("toes^2=%d\n",toes*toes);
+//     printf("toes^3=%d\n",toes*toes*toes);
+// }
+//7
+// void h(void);
+// int main(void)
+// {
+//     h();
+//     h();
+//     h();
+//     printf("\n");
+//     h();
+//     h();
+//     printf("\n");
+//     h();
+// }
+// void h(void)
+// {
+//     printf("smile!");
+// }
+//8
+// void one_three(void);
+// void two(void);
+// int main(void)
+// {
+//     printf("starting now!\n");
+//     one_three();
+//     printf("done!");
+// }
+// void one_three(void)
+// {
+//     printf("one\n");
+//     two();
+//     printf("\nthree\n");
+// }
+// void two(void)
+// {
+//     printf("two");
+// }
